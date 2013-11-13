0969701f9f384bdde12327ec4fbacbb0e7a33be3
Edilson Gonzalez <edilson.gonza@unitec.edu>
2013-11-13 07:43:26 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+<html lang="es">
+<head>
+	<title>Bookstore</title>
+	<meta charset="utf-8">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-navbar">
+			    <span class="sr-only">Toggle navigation</span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			</button>
+		   <a class="navbar-brand" href="index.php">Bookstore</a>
+		</div>
+
+		<div class="collapse navbar-collapse" id="main-navbar">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Compañía</a></li>
+				<li><a href="#">Contáctenos</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!-- JQuery -->
+	<script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
+	<!-- Bootstrap JS -->
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+</body>
+</html>
